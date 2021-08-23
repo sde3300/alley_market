64bc0c7e6450f9f726001b90d2e99949dd3d18ef
sde3300 <sde3300@naver.com>
2021-08-23 11:30:56 +0900

    210823 11:30

---
+++ src/main/java/am/AlleyMarketApplication.java
@@ -0,0 +1,13 @@
+package am;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlleyMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlleyMarketApplication.class, args);
+	}
+
+}
