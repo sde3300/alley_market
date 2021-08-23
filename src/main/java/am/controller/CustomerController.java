c2ebadaca50616e11cce86c9f83c44af4d9318d0
sde3300 <sde3300@naver.com>
2021-08-23 14:38:20 +0900

    0823 14:38

--- src/main/java/am/controller/CustomerController.java
+++ src/main/java/am/controller/CustomerController.java
@@ -1,5 +1,26 @@
 package am.controller;
 
-public class CustomerController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import am.dto.ProductDto;
+import am.service.CustomerService;
+
+@RestController
+public class CustomerController {
+	
+	@Autowired
+	private CustomerService cusService;
+	
+//	메인페이지 불러오기
+	@RequestMapping(value="/productMain", method=RequestMethod.GET)
+	public Object productMainPage() throws Exception {
+		List<ProductDto> dataList = cusService.productMainPage();
+		
+		return dataList;
+	}
 }
