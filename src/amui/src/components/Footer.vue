b186a3b3d5743474d15c96942ba0084bd6d9a1f2
sde3300 <sde3300@naver.com>
2021-09-02 15:16:58 +0900

    푸터

--- src/amui/src/components/Footer.vue
+++ src/amui/src/components/Footer.vue
@@ -2,44 +2,57 @@
     <div class="footer">
     <hr>
     <div class="container">
-        <div class="row align-items-center">
-            <div class="col-sm">
+        
+            <div class="row">
+                <div class="col">
                     <p>상호명 : 골목식당 | 대표자: *** | 사업자번호 : 123-32-12345</p>
                     <p>주소 : 부산광역시 수영구 ***</p>
                     <p>입접문의 : *** | 이메일 주소: golmok@hanmail.net</p>
-                <div class="container">
                 </div>
+            
+                <div class="col">
+                    <p>고객센터 1667-1235</p>
+                    <p>평일 09:00 ~ 18:00 점심시간 12:00-13:00</p>
+                    <p>주말 및 공휴일 휴무</p>
+                </div>
+            
+                <div class="col-3">
+                    <span>
+                    <ul>
+                        <li>
+                            <a href="https://www.instagram.com/" class="iconlink">
+                            <span style="font-size: 2em; ">
+                                <i class="fab fa-instagram"></i>
+                            </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/" class="iconlink">
+                            <span style="font-size: 2em; ">
+                                <i class="fab fa-facebook-f"></i>
+                            </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/?lang=ko" class="iconlink">
+                            <span style="font-size: 2em; ">
+                                <i class="fab fa-twitter"></i>
+                            </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.youtube.com/" class="iconlink">
+                            <span style="font-size: 2em; ">
+                                <i class="fab fa-youtube"></i>
+                            </span>
+                            </a>
+                        </li>
+                    </ul>
+                    </span>
+                </div>
+                
             </div>
-            <div class="col-sm">
-                <p>고객센터 1667-1235</p>
-                <p>평일 09:00 ~ 18:00 점심시간 12:00-13:00</p>
-                <p>주말 및 공휴일 휴무</p>
-            </div>
-            <div class="col-sm">
-                <ul>
-                    <li>
-                        <span style="font-size: 2em; ">
-                            <i class="fab fa-instagram"></i>
-                        </span>
-                    </li>
-                    <li>
-                        <span style="font-size: 2em; ">
-                            <i class="fab fa-facebook-f"></i>
-                        </span>
-                    </li>
-                    <li>
-                        <span style="font-size: 2em; ">
-                            <i class="fab fa-twitter"></i>
-                        </span>
-                    </li>
-                    <li>
-                        <span style="font-size: 2em; ">
-                            <i class="fab fa-youtube"></i>
-                        </span>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        
     </div>
     <!-- <div class="container">footer 입니다</div> -->
 
@@ -67,4 +80,5 @@ ul li {
     margin-left: 10px;
     }
 
+.iconlink {color: black;}
 </style>
